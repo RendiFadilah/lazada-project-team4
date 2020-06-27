1de8ae8f79149ee9fb29e02a978e1383219ef3a3
FuryNashwaAspirna <FurryAspirnaa11@gmail.com>
2020-06-27 14:38:13 +0700

    update halaman login penjual

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import "./assets/scss/style.scss";
 import "./assets/scss/style-search.scss";
 import LandingPage from "./pages/LandingPage"
-import LoginPenjual from './pages/auth/login/LoginPenjual';
+import LoginPenjual from "./pages/auth/login/LoginPenjual";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Route exact path="/" component={LandingPage}></Route>
         <Route path="/login" component={LoginPenjual}></Route>
       </Router>
     </div>
