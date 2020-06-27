aeadfe18cd7fb4083dbe3e7104da93c02731c4ce
MAzisSaputra <muhammadazis41103@gmail.com>
2020-06-27 13:08:50 +0700

    update folder backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const user = [
+  {
+    "name": "azis",
+  }
+]
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/login', function(req, res, next) {
+  res.sends(user)
 });
 
 module.exports = router;
