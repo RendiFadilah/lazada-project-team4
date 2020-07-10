58141ea052387b6a47ec9876f6603aaf780f6c82
najwa 81 <najwabajabir@gmail.com>
2020-07-10 15:51:45 +0700

    add register penjual

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,27 +5,17 @@ import "./assets/scss/style.scss";
 import LandingPage from "./pages/LandingPage"
 
 import LoginPenjual from './pages/auth/login/LoginPenjual';
-<<<<<<< HEAD
 import LoginPembeli from './pages/auth/login/LoginPembeli'
-=======
-import LandingApi from './api/LandingApi';
-
-
->>>>>>> 062e04d5aa4bd36f31ed7f7ef7f75b59b4e5e923
+import RegisterPembeli from './pages/auth/register/RegisterPenjual'
 function App() {
   return (
 
 
     <div className="App">
       <Router>
-<<<<<<< HEAD
         <Route exact path="/" component={LandingPage}></Route>
         <Route path="/login-pembeli" component={LoginPembeli}></Route>
-=======
-        <Route exatch path="/" component={LandingPage}></Route>
-        <Route path="/login" component={LoginPenjual}></Route>
-        <Route path="/api" component={LandingApi}></Route>
->>>>>>> 062e04d5aa4bd36f31ed7f7ef7f75b59b4e5e923
+        <Route path="/register-penjual" component={RegisterPembeli}></Route>
       </Router>
     </div>
   );
